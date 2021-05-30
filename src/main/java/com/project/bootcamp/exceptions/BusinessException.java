d319a571352ed85cb1b95a6fd27d54b720943a1a
Matrix <dida_fernando#hotmail.com>
2021-05-29 23:56:20 -0300

    Implementado Exceptions Handlers

--- src/main/java/com/project/bootcamp/exceptions/BusinessException.java
+++ src/main/java/com/project/bootcamp/exceptions/BusinessException.java
@@ -1,4 +1,8 @@
 package com.project.bootcamp.exceptions;
 
-public class BusinessException {
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
 }
